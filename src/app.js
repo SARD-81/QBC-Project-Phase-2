f4c0836be8814861d5fa4c9508571fdb8abaf788
SARD-81 <davarzaniamirreza@gmail.com>
2025-05-06 15:54:41 +0330

    ui design almost compelete and oop aded

--- src/app.js
+++ src/app.js
@@ -47,11 +47,132 @@ lightBtn.addEventListener("click", () => {
 }); // set the them by click
 /* finish handle dark mode of website */
 
-document.addEventListener("DOMContentLoaded", () => {
-  const tagButton = document.getElementById("tag-button");
-  const tagOptions = document.getElementById("tag-options");
+/* start handle the add task modal */
+const siblings = (elem) => {
+  // create an empty array
+  let siblings = [];
 
-  tagButton.addEventListener("click", () => {
-    tagOptions.classList.toggle("hidden");
+  // if no parent, return empty list
+  if (!elem.parentNode) {
+    return siblings;
+  }
+
+  // first child of the parent node
+  let sibling = elem.parentNode.firstElementChild;
+
+  // loop through next siblings until `null`
+  do {
+    // push sibling to array
+    if (sibling != elem) {
+      siblings.push(sibling);
+    }
+  } while ((sibling = sibling.nextElementSibling));
+
+  return siblings;
+};
+
+$.querySelector("#add-new-sec").addEventListener("click", () => {
+  $.querySelector("#add-tasks-btn").classList.add("hidden");
+  $.querySelector("#img-holder").classList.add("hidden");
+  $.querySelector("#add-task-form").classList.remove("hidden");
+});
+
+$.querySelector("#close-form").addEventListener("click", resetAddTaskForm);
+
+$.getElementById("tag-button").addEventListener("click", () => {
+  $.getElementById("tag-options").classList.toggle("hidden");
+  $.getElementById("unselected-svg").classList.toggle("hidden");
+  $.getElementById("selected-svg").classList.toggle("hidden");
+});
+
+$.querySelector("#task-item-menu").addEventListener("click", () => {
+  $.querySelector("#task-item-ops").classList.toggle("hidden");
+});
+
+const priorityBtns = Array.from($.querySelectorAll(".priority-btn"));
+
+priorityBtns.map((items) => {
+  items.addEventListener("click", () => {
+    $.querySelector("#tags-container").classList.add("hidden");
+    const contColor = items.classList[1];
+    siblings(items).map((item) => {
+      item.classList.add("hidden");
+      if (item.tagName === "A") {
+        item.classList.remove("hidden");
+      }
+    });
+    $.querySelector("#tag-options").classList.remove("px-3");
+    $.querySelector("#tag-options").classList.remove("py-2");
+    $.querySelector("#tag-options").classList.remove("border-1");
+    $.querySelector("#tag-options").classList.remove("border-gray-300");
+    $.querySelector("#tag-options").classList.add(contColor);
+    $.querySelector(".close").classList.remove("hidden");
   });
 });
+
+function resetAddTaskForm() {
+  $.querySelector("#add-tasks-btn").classList.remove("hidden");
+  $.querySelector("#add-task-form").classList.add("hidden");
+  $.getElementById("tag-options").classList.add("hidden");
+  $.getElementById("selected-svg").classList.add("hidden");
+  $.getElementById("unselected-svg").classList.remove("hidden");
+  $.querySelector("#tags-container").classList.remove("hidden");
+  $.querySelector("#tag-options").classList.add("px-3");
+  $.querySelector("#tag-options").classList.add("py-2");
+  $.querySelector("#tag-options").classList.add("border-1");
+  $.querySelector("#tag-options").classList.add("border-gray-300");
+  priorityBtns.map((items) => {
+    siblings(items).map((item) => {
+      item.classList.remove("hidden");
+    });
+  });
+
+  let elemClass = [...$.querySelector("#tag-options").classList];
+  let filterElemClass = elemClass.filter((a) => !a.includes("bg-"));
+  $.querySelector("#tag-options").setAttribute("class", "");
+  filterElemClass.map((cl) => {
+    $.querySelector("#tag-options").classList.add(cl);
+  });
+  $.querySelector(".close").classList.add("hidden");
+}
+/* finish handle the add task modal */
+/////////////////////////////////
+$.querySelector("#add-task-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+/////////////////////////////////
+
+class Tasks {
+  static count = 0; //a variable for counting the instances of class (to set id for every class to easily mmanage them)
+  taskTitle; //Title of tasks
+  taskDescription; //Description of tasks
+  taskTag; //Tag of tasks
+  taskIsDone = false; //flag of task that its done or not
+
+  /* the constructor of class */
+  constructor(taskTitle, taskDescription, taskTag) {
+    this.taskId = ++Tasks.count;
+    this.taskTitle = taskTitle;
+    this.taskDescription = taskDescription;
+    this.taskTag = taskTag;
+  }
+
+  isDone() {
+    //we use if to detect the dom that task is tiked or not
+    //and if is tiked we call this method for instances and
+    // change the dom with this method
+  }
+
+  deleteTasks() {}
+
+  showTask() {
+    console.log(
+      `${this.taskId} the task is ${this.taskTitle} and desc is ${this.taskDescription} and the tag is ${this.taskTag}`
+    );
+  }
+}
+
+const newTask = new Tasks("buyying", "go to shop and buy apple", "high");
+
+newTask.showTask();
+console.log(newTask);
